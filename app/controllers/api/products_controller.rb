73bd59ee3ecfa6b77ddd49adc473bfc8b8f8b244
chuck1193 <>
2019-01-18 12:31:44 -0600

    mini_capstone

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,41 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render 'products.json.jbuilder'
+  end
+
+
+  def create
+    @product = Product.new(
+                            name: params[:name],
+                            price: params[:price],
+                            description: params[:description],
+                            image_url: params[:image_url]
+                          )
+    prodcut.save
+    render 'show.json.jbuilder'
+  end
+  def show
+    product_id = Product params[:id]
+    @product = Product.find(product_id)
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @rproduct.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
+
+    product.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Successfully removed recipe"}
+  end
 end
