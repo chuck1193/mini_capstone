4c465e6b2afef7ea08feb0ca5adca6cbf467d53a
chuck1193 <>
2019-02-01 09:32:48 -0600

    ProductCategory

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,20 @@
 #   {name: "Guinea Pig", price: "75.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5zaivUamwXSF0AE7f7JSYz1si3YLTOnLNmOZQOxcphw9ID4wX", description: "Long haired guinea pig for sale. Loves runnning on a wheel and munching on snacks. If you love to snack as well, you have found your new buddy!", in_stock: true},
 #   {name: "Pig", price: "300.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMy_xDkepFkGZU2slq-ZbaveLNdOIrZFBQ8uVRVusMyGaxw5aZBw", description: "Sweet friend that will love you for the rest of its life. Very intelligent.", in_stock: true}
 # ])
-Category.create(name: "Small Animals")
 
-ProductCatergory.create(product_id: 1, category_id: 1)
-ProductCatergory.create(product_id: 2, category_id: 1)
-ProductCatergory.create(product_id: 3, category_id: 1)
-ProductCatergory.create(product_id: 4, category_id: 1)
-ProductCatergory.create(product_id: 5, category_id: 1)
-ProductCatergory.create(product_id: 6, category_id: 1)
+
+
+# Category.create(name: "Small Animals")
+# Category.create(name: "Wild Animals")
+
+
+# ProductCatergory.create(product_id: 2, category_id: 1)
+# ProductCatergory.create(product_id: 3, category_id: 2)
+# ProductCatergory.create(product_id: 1, category_id: 1)
+# ProductCatergory.create(product_id: 5, category_id: 2)
+# ProductCatergory.create(product_id: 4, category_id: 1)
+
+
+
+
+
